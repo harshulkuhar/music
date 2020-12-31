e2aef17dda59eac527338f0287e2da2cf3a85282
Harshul Kuhar <harshul.kuhar@gmail.com>
2020-12-31 17:59:24 +0530

    Added unfinished comments to SongService interface

--- src/main/java/com/music/song/SongService.java
+++ src/main/java/com/music/song/SongService.java
@@ -9,11 +9,15 @@ public interface SongService {
     /**Returns all songs belonging to a particular album */
     public List<Song> getAllSongsbyAlbum(String albumid);
 
+    /**Get a Song object given its id */
     public Song getSong(String id);
 
+    /** Add a song to the database. */
     public void addSong(Song song);
 
+    /**Edit or update a song. */
     public void editSong(Song song);
 
+    /**Delete a song from the database. */
     public void deleteSong(String id);
 }
