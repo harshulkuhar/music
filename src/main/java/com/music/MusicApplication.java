366fb5c851ec9f94b2ae4ff4838f27543795034f
Harshul Kuhar <harshul.kuhar@gmail.com>
2021-01-11 14:37:38 +0530

    Moved api to a folder above - removed security for now

--- src/main/java/com/music/MusicApplication.java
+++ src/main/java/com/music/MusicApplication.java
@@ -1,5 +1,6 @@
 package com.music;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
--- src/main/java/com/music/model/Album.java
+++ src/main/java/com/music/model/Album.java
@@ -1,4 +1,4 @@
-package com.music.album;
+package com.music.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
